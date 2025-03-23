f919b4668af85e8b78d78d92e361d7b8c2e84649
Rohit Jena <168836445+RohitJena1075@users.noreply.github.com>
2025-03-23 22:35:20 +0530

    Delete footer.js

--- footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.html") // Load footer.html dynamically
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-placeholder").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading footer:", error));
-});
-
