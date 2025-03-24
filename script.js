79c33a1abcd8d48021242f6a96fb3e7a5f14edee
Rohit Jena <168836445+RohitJena1075@users.noreply.github.com>
2025-03-24 19:28:10 +0000

    Change in take a quiz

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-<script>
-    history.scrollRestoration = "manual";
-</script>
+function scrollCarousel(id, direction) {
+    const container = document.getElementById(id);
+    container.scrollBy({
+        left: direction * 300,
+        behavior: 'smooth'
+    });
+}
+
